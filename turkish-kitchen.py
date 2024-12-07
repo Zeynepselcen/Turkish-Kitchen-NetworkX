239e600ccccf586646ddc3e83f41178151b74dca
ismailseker <sseker665@gmail.com>
2024-12-07 14:02:30 +0300

    meltem's part

--- turkish-kitchen.py
+++ turkish-kitchen.py
@@ -41,11 +41,36 @@ for node, data in G.nodes(data=True):
 print("Kenarlar ve Özellikler:")
 for edge in G.edges(data=True):
     print(edge)
+    
+# En fazla komşusu olan yemekleri bulma
+degree_dict = dict(G.degree())  # Her düğümün derecesini (komşu sayısı) al
+max_degree = max(degree_dict.values())  # Maksimum dereceyi bul
+
+# En fazla komşusu olan düğümleri listeleme
+most_connected_nodes = [node for node, degree in degree_dict.items() if degree == max_degree]
+
+# En fazla komşusu olan 5 yemeği bulma
+sorted_degree = sorted(degree_dict.items(), key=lambda x: x[1], reverse=True)
+top_5_nodes = sorted_degree[:5]
+                            
+
+print(f"\nEn fazla komşusu olan yemekler:")
+for node in most_connected_nodes:
+    print(f"Yemek ID: {node}, Yemek Adı: {G.nodes[node]['name']}, Komşu Sayısı: {max_degree}")
+
+print(f"\nEn fazla komşusu olan 5 yemek:")
+for node, degree in top_5_nodes:
+    yemek = G.nodes[node]
+    print(f"Yemek ID: {node}, Yemek Adı: {yemek['name']}, Komşu Sayısı: {degree}")
+
+pos = nx.spring_layout(G, seed=42)
+
 
 # Grafiği görselleştirme
 plt.figure(figsize=(12, 12))
 nx.draw(
     G,
+    pos=pos,
     with_labels=True,
     node_size=3000,
     font_size=10,
